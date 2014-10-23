ae3bd6b40544b351b69f3cfd3fa145755e7432f3
Jeffrey Terrell Young <youngj4@football.cs.byu.edu>
2014-10-22 22:14:16 -0600

    added web.conf parser

---
+++ config-file-parser.py
@@ -0,0 +1,20 @@
+class ParsedConfigFile:
+	def __init__(self):
+		self.medias = {};
+		self.hosts = {};
+		self.parameters = {};
+		self.parseConfigFile();
+	def parseConfigFile(self):
+		with open('web.conf') as f:
+			for line in f:
+				self.parseLine(line.rstrip())
+	def parseLine(self, line):
+		words = line.split(" ", 2)
+		if "host" in words[0]:
+			self.hosts[words[1]] = words[2]
+		elif "media" in words[0]:
+			self.medias[words[1]] = words[2]
+		elif "parameter" in words[0]:
+			self.parameters[words[1]] = words[2]
+
+ParseConfigFile()
