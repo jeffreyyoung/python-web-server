44dd24bbd8c301d94320d98b2cc15dba51c44fb0
Jeffrey Terrell Young <youngj4@football.cs.byu.edu>
2014-10-22 21:36:42 -0600

    added http parser

---
+++ http-request-parser.py
@@ -0,0 +1,34 @@
+class ParsedHttpRequest:
+	def __init__(self, request):
+		self.isValid = False
+		self.statusCode = None
+		self.parse_request(request)
+	def parse_request(self, request):
+
+		try:
+			message = request.split('\r\n\r\n', 1)
+			lines = message[0].split("\r\n")
+			requestLineWords = lines[0].split()
+			self.method = requestLineWords[0]
+			self.url = requestLineWords[1]
+			self.version = requestLineWords[2]
+			self.headers = {}
+			for i in range(1, len(lines)):
+				splitheader = lines[i].split(" ", 1)
+				self.headers[splitheader[0]] = splitheader[1]
+
+			if self.headers[]
+
+			self.isValid = True
+			self.statusCode = 200
+		except:
+			self.isValid = False
+			self.statusCode = 400
+
+
+
+
+request = "GET www.google.com HTTP1.1\r\nheadfield1 value1\r\nheadfield2 value2\r\n\r\n"
+
+r = ParsedHttpRequest(request)
+print r.isValid, r.statusCode, r.method, r.url, r.version, r.headers
